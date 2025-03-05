8f53eb305f329a806476046c00c113621040d989
Jennifer Fu <jenniferfu03@gmail.com>
2025-03-04 21:29:38 -0500

    initial commit, created navbar and fnished homepage

---
+++ server.py
@@ -0,0 +1,77 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+app = Flask(__name__)
+
+dining_halls = [
+    {
+        "id": 1,
+        "name": "Ferris Booth Commons",
+        "image": "https://dining.columbia.edu/sites/default/files/styles/cu_crop/public/content/FBC%20location%20photos/Ferris%20dessert%20case.jpg?itok=wDKiUcPS"
+    },
+    {
+        # CHANGE ALL OF THESE IMAGES!!! TODO
+        "id": 2,
+        "name": "John Jay Dining Hall",
+        "image": "https://dining.columbia.edu/sites/default/files/styles/cu_crop/public/content/FBC%20location%20photos/Ferris%20dessert%20case.jpg?itok=wDKiUcPS"
+    },
+    {
+        "id": 3,
+        "name": "JJ's Place",
+        "image": "https://dining.columbia.edu/sites/default/files/styles/cu_crop/public/content/FBC%20location%20photos/Roaree-73v2.jpg?itok=h3-Mw9Zt"
+    },
+    {
+        "id": 4,
+        "name": "Hewitt Dining Hall",
+        "image": "https://dining.columbia.edu/sites/default/files/styles/cu_crop/public/content/FBC%20location%20photos/Roaree-73v2.jpg?itok=h3-Mw9Zt"
+    },
+    {
+        "id": 5,
+        "name": "Diana Center Café",
+        "image": "https://i.pinimg.com/originals/6f/24/e3/6f24e398413843a17cba7f42daccac56.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Chef Mike's Sub Shop",
+        "image": "https://dining.columbia.edu/sites/default/files/styles/cu_crop/public/content/FBC%20location%20photos/Roaree-73v2.jpg?itok=h3-Mw9Zt"
+    },
+    {
+        "id": 7,
+        "name": "Faculty House",
+        "image": "https://dining.columbia.edu/sites/default/files/styles/cu_crop/public/content/FBC%20location%20photos/Roaree-73v2.jpg?itok=h3-Mw9Zt"
+    },
+    {
+        "id": 8,
+        "name": "Grace Dodge Dining Hall",
+        "image": "https://dining.columbia.edu/sites/default/files/styles/cu_crop/public/content/FBC%20location%20photos/Roaree-73v2.jpg?itok=h3-Mw9Zt"
+    },
+    {
+        "id": 9,
+        "name": "Chef Don's Pizza Pi",
+        "image": "https://dining.columbia.edu/sites/default/files/styles/cu_crop/public/content/FBC%20location%20photos/Roaree-73v2.jpg?itok=h3-Mw9Zt"
+    },
+    {
+        "id": 10,
+        "name": "Café East",
+        "image": "https://dining.columbia.edu/sites/default/files/styles/cu_crop/public/content/FBC%20location%20photos/Roaree-73v2.jpg?itok=h3-Mw9Zt"
+    },
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html', dining_halls=dining_halls)
+
+@app.route('/view/<id>')
+def view_id(id=id): # how is id this passed in idk
+    return render_template('view_id.html', id=id, dining_halls=dining_halls)
+
+@app.route('/search_results')
+def search_results():
+    query = request.args.get('q')
+    return "query: " + query
+    # TODO
+    # route that takes in searched info, searches thru all data,
+    # returns only matching items to a view that shows list of search results
+
+if __name__ == '__main__':
+   app.run(debug = True, port=5001)
---
+++ static/index.js
@@ -0,0 +1,27 @@
+function displayThree(topHalls) {
+    // places images in columns
+    for (let i = 0; i < topHalls.length; i++) {
+        let hall = halls.find(h => h.id === topHalls[i]);
+        let newCol = $("<div class='col-4'>")
+        let hallLink = $("<a>").attr("href", "/view/" + hall.id);
+        $(newCol).append((hallLink).append($("<img>").attr("src", hall.image)
+            .attr("alt", hall.name).addClass("home-image")));
+        $(".home-images").append(newCol)
+    }
+    // places names under images in columns
+    for (let i = 0; i < topHalls.length; i++) {
+        let hall = halls.find(h => h.id === topHalls[i]);
+        let newCol = $("<div class='col-4'>")
+        $(newCol).append($("<a>").attr("href", "/view/" + hall.id).text(hall.name));
+        $(".home-text").append(newCol)
+    }
+}
+
+// when page loads, display top 3 dining halls
+$(document).ready(function(){
+    displayThree([1, 3, 5]); // change here to customize!!
+
+
+
+
+})
